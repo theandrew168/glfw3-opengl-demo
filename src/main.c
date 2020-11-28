f3a8960cd1f302a886b3d41ec10e6f268d673a6c
theandrew168 <steveday168@gmail.com>
2020-11-28 12:12:44 -0600

    Fix macOS native build

--- src/main.c
+++ src/main.c
@@ -125,6 +125,7 @@ main(int argc, char* argv[])
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE);
     GLFWwindow* window = glfwCreateWindow(640, 640, "GLFW3 OpenGL Demo", NULL, NULL);
     if (window == NULL) {
         const char* error = NULL;
